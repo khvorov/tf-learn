708b6f7294aeceb05742fb31ca8e0f3669f22c17
Dmitry Khvorov <dmitry.khvorov@gmail.com>
2017-05-29 23:48:07 +0800

    sdc: first cut

---
+++ sdc/sd_model.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import pandas as pd
+
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Lambda, Conv2D, Dropout, Flatten, Dense
+from keras.models import Sequential
+from keras.optimizers import Adam
+
+from sklearn.model_selection import train_test_split
+
+from utils import INPUT_SHAPE, batch_generator
+
+def load_data(args):
+    data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'), header=None, names=['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed'])
+
+    X = data_df[['center', 'left', 'right']].values
+    y = data_df['steering'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=0)
+
+    return X_train, X_test, y_train, y_test
+
+def build_model(args):
+    # Nvidia self-driving car model
+
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=INPUT_SHAPE))
+    model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
+    model.add(Conv2D(filters=36, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
+    model.add(Conv2D(filters=48, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='elu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='elu'))
+    model.add(Dropout(rate=args.keep_prob))
+    model.add(Flatten())
+    model.add(Dense(units=100, activation='elu'))
+    model.add(Dense(units=50, activation='elu'))
+    model.add(Dense(units=10, activation='elu'))
+    model.add(Dense(units=1))
+
+    return model
+
+def train_model(args, model, X_train, X_test, y_train, y_test):
+    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5', monitor='val_loss', verbose=0, save_best_only=args.save_best_only, mode='auto')
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
+
+    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
+                        args.samples_per_epoch,
+                        args.n_epochs,
+#                        max_q_size=1,
+                        validation_data=batch_generator(args.data_dir, X_test, y_test, args.batch_size, False),
+                        validation_steps=len(X_test),
+                        callbacks=[checkpoint],
+                        verbose=1)
+
+def s2b(s):
+    s = s.lower(s)
+    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
+
+def main():
+    parser = argparse.ArgumentParser(description='Self Driving Car Model')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
+    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
+    parser.add_argument('-k', help='dropout probability',   dest='keep_prob',         type=float, default=0.5)
+    parser.add_argument('-n', help='number of epochs',      dest='n_epochs',          type=int,   default=10)
+    parser.add_argument('-s', help='samples per epochs',    dest='samples_per_epoch', type=int,   default=20000)
+    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
+    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=bool,  default=True)
+    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1e-4)
+
+    args = parser.parse_args()
+
+    # print parameters
+    print('=' * 30)
+    print('Parameters:')
+    print('=' * 30)
+    for key, value in vars(args).items():
+        print('{:<20} := {}'.format(key, value))
+    print('=' * 30)
+
+    data = load_data(args)
+    model = build_model(args)
+    model.summary()
+
+    train_model(args, model, *data)
+
+if __name__ == '__main__':
+    main()
+
---
+++ sdc/utils.py
@@ -0,0 +1,99 @@
+import cv2
+import matplotlib.image as mpimg
+import numpy as np
+import os
+
+ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH = 160, 320
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 80, 160, 3
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+class ImageLoader:
+    def __init__(self, data_dir, image_paths):
+        n_images = image_paths.shape[0]
+        self.n_images = n_images
+
+        print('Loading %d images from %s...' % (n_images, data_dir))
+
+        self.center = np.empty([n_images, ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH, IMAGE_CHANNELS], dtype=np.uint8)
+        self.left = np.empty([n_images, ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH, IMAGE_CHANNELS], dtype=np.uint8)
+        self.right = np.empty([n_images, ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH, IMAGE_CHANNELS], dtype=np.uint8)
+        
+        for idx, img in enumerate(image_paths):
+            center, left, right = img
+            self.center[idx] = self._load_image(data_dir, center)
+            self.left[idx]   = self._load_image(data_dir, left)
+            self.right[idx]  = self._load_image(data_dir, right)
+
+        print('Loaded %d images from %s...' % (n_images, data_dir))
+
+    def _load_image(self, data_dir, image_file):
+        return mpimg.imread(os.path.join(data_dir, image_file))
+
+# preprocess an image
+def crop(image):
+    return image[60:-25, :]
+
+def resize(image):
+    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
+
+def rgb2yuv(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+
+def preprocess(image):
+    return rgb2yuv(resize(image))
+
+# augument the image
+def choose_image(image_loader, idx, steering_angle):
+    choice = np.random.choice(3)
+    if choice == 0:
+        return image_loader.left[idx], steering_angle + 0.2
+    elif choice == 1:
+        return image_loader.right[idx], steering_angle - 0.2
+    return image_loader.center[idx], steering_angle
+
+def random_flip(img):
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+    return img
+
+def random_translate(img, range_x, range_y):
+    trans_x = range_x * (np.random.rand() - 0.5)
+    trans_y = range_y * (np.random.rand() - 0.5)
+    trans_m = np.float32([[1, 0, trans_x],
+                          [0, 1, trans_y]])
+    h, w = img.shape[:2]
+    return cv2.warpAffine(img, trans_m, (w, h))
+
+def random_brightness(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    ratio = 1.0 + 0.2 * (np.random.rand() - 0.5)
+    hsv[:, :, 2] = hsv[:, :, 2] * ratio
+    return cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+
+def augument(img, range_x=100, range_y=10):
+    img = random_flip(img)
+    img = random_brightness(img)
+    img = random_translate(img, range_x, range_y)
+    return img
+
+def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_training):
+    il = ImageLoader(data_dir, image_paths)
+
+    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+    steers = np.empty(batch_size)
+
+    while True:
+        i = 0
+        for index in np.random.permutation(il.n_images):
+            steering_angle = steering_angles[index]
+            if is_training:
+                image, steering_angle = choose_image(il, index, steering_angle)
+            else:
+                image = il.center[index]
+            images[i] = preprocess(image)
+            steers[i] = steering_angle
+            i += 1
+            if i == batch_size:
+                break
+        yield images, steers
+
